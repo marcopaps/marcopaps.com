f1282ac86b5db45569cf914653fcfd89e1b04183
Marco Budiongan <marcobdngn@gmail.com>
2022-01-18 14:20:24 +0800

    Setup basic storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
+import { DefaultButton } from "@/components/Button";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <DefaultButton label={"Hello there!"} />;
 };
 
 export default Home;
