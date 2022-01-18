aa71e89f99e3f57f030fb3ef521406f29a536d6b
Marco Budiongan <marcobdngn@gmail.com>
2022-01-18 17:32:31 +0800

    Updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { DefaultButton } from "@/components/Button";
 
 const Home: NextPage = () => {
-  return <DefaultButton label={"Hello there!"} />;
+  return <h1 className="text-2xl text-center m-32">Under construction...</h1>;
 };
 
 export default Home;
