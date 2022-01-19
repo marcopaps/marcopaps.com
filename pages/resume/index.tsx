596a5d8dc9db3cd0bc19611f8e8c25195ee74d51
Marco Budiongan <marcobdngn@gmail.com>
2022-01-19 15:52:02 +0800

    Add basic resume data fetching

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,5 +1,33 @@
-const Resume = () => {
-  return <div>{"Resume"}</div>;
+import { useCallback } from "react";
+import { Client } from "@/contentfulApi";
+import { ContentTypes } from "@/types/contentTypesEnum";
+
+import type { NextPage } from "next";
+import type { IJobExperience } from "@/types/generated/contentful";
+
+interface PropsType {
+  jobExperiences: IJobExperience[];
+}
+
+export async function getStaticProps() {
+  const jobExperiences = await Client.getEntries({
+    content_type: ContentTypes.JOB_EXPERIENCE,
+  });
+
+  return {
+    props: { jobExperiences: jobExperiences.items },
+    revalidate: 1,
+  };
+}
+
+const Resume: NextPage<PropsType> = ({ jobExperiences }) => {
+  const renderJobExperiences = useCallback(() => {
+    return jobExperiences.map((item) => {
+      return <p key={item.sys.id}>{item.fields.company}</p>;
+    });
+  }, [jobExperiences]);
+
+  return <div>{renderJobExperiences()}</div>;
 };
 
 export default Resume;
--- types/contentTypesEnum.ts
+++ types/contentTypesEnum.ts
@@ -1,4 +1,8 @@
+// This needs to be updated to use CONTENT_TYPE from Contentful literal types
+// Will work if project is not too complicated
+
 export enum ContentTypes {
   HOBBY = "hobby",
   PHOTOS = "photos",
+  JOB_EXPERIENCE = "jobExperience",
 }
