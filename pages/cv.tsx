dcd4605cdcdacf8bc7071e435c6f3a4ee023fbf7
Marco Budiongan <marcobdngn@gmail.com>
2022-02-09 16:47:20 +0800

    Updated CV details

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -8,25 +8,54 @@ import {
 } from "@/components";
 
 import type { NextPage } from "next";
-import type { IJobExperience } from "@/types/generated/contentful";
+import type {
+  IHobby,
+  IJobExperience,
+  ISkill,
+} from "@/types/generated/contentful";
 
-interface PropsType {
+interface IProps {
   jobExperiences: IJobExperience[];
+  skills: ISkill[];
+  hobbies: IHobby[];
 }
 
-export async function getStaticProps() {
-  const jobExperiences = await Client.getEntries({
+const contentfulEntries = [
+  {
     content_type: ContentTypes.JOB_EXPERIENCE,
     order: "-fields.order",
-  });
+  },
+  {
+    content_type: ContentTypes.SKILL,
+    order: "-fields.rating",
+  },
+  {
+    content_type: ContentTypes.HOBBY,
+    order: "-fields.title",
+  },
+];
+
+export async function getStaticProps() {
+  const [jobExperiences, skills, hobbies] = await Promise.all(
+    contentfulEntries.map((query) =>
+      Client.getEntries({
+        content_type: query.content_type,
+        order: query.order,
+      })
+    )
+  );
 
   return {
-    props: { jobExperiences: jobExperiences.items },
+    props: {
+      jobExperiences: jobExperiences.items,
+      skills: skills.items,
+      hobbies: hobbies.items,
+    },
     revalidate: 1,
   };
 }
 
-const Resume: NextPage<PropsType> = (props) => {
+const Resume: NextPage<IProps> = (props) => {
   return (
     <div>
       <CVHeader className="sticky inset-0 z-10" />
@@ -45,7 +74,7 @@ const Resume: NextPage<PropsType> = (props) => {
           />
         </div>
         <div className="border-l-2 p-4">
-          <CVPersonalDetails />
+          {<CVPersonalDetails skills={props.skills} hobbies={props.hobbies} />}
         </div>
       </div>
     </div>
