f69570c23ac5cbc17db74c08832602a1a74e890f
Marco Budiongan <marcobdngn@gmail.com>
2022-02-10 14:08:09 +0800

    Add icon helper

---
+++ icons/getIcon.tsx
@@ -0,0 +1,15 @@
+import {
+  IconFB,
+  IconGithub,
+  IconInstagram,
+  IconLinkedIn,
+  IconTwitter,
+} from ".";
+
+export const getIcon = (icon: string, fill?: string) => {
+  if (icon === "facebook") return <IconFB fill={fill} />;
+  if (icon === "linkedin") return <IconLinkedIn fill={fill} />;
+  if (icon === "twitter") return <IconTwitter fill={fill} />;
+  if (icon === "github") return <IconGithub fill={fill} />;
+  if (icon === "instagram") return <IconInstagram fill={fill} />;
+};
---
+++ icons/index.ts
@@ -0,0 +1,5 @@
+export { default as IconFB } from "public/facebook.svg";
+export { default as IconLinkedIn } from "public/linkedin.svg";
+export { default as IconGithub } from "public/github.svg";
+export { default as IconTwitter } from "public/twitter.svg";
+export { default as IconInstagram } from "public/instagram.svg";
