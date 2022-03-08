77efd2299e0d1f71b7bbaf6e015a06096cb9e802
Marco Budiongan <marcobdngn@gmail.com>
2022-03-08 12:46:17 +0800

    Remove hard coded serverless enpoint

--- utils/addSiteVisitCount.ts
+++ utils/addSiteVisitCount.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export async function addSiteVisitCount() {
+  const { SERVERLESS_ENDPOINT, SERVERLESS_SITE_COUNTER_FUNCTION } = process.env;
   try {
     return axios.post(
-      'https://marcopapsserverless.azurewebsites.net/api/AddSiteVisitCount',
+      `${SERVERLESS_ENDPOINT}/${SERVERLESS_SITE_COUNTER_FUNCTION}`,
       {}
     );
   } catch (error) {
