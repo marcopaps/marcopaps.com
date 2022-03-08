f046c20dbc72d21d24231e4471c7671a8964930a
Marco Budiongan <marcobdngn@gmail.com>
2022-03-08 13:41:57 +0800

    Update on footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,10 +1,8 @@
-const Footer = () => {
+export default function Footer() {
   return (
     <footer className="border-t-2 py-4 text-center text-gray-500">
-      <>&#169; </>
+      <>&copy; </>
       {'2022 Marco Budiongan. All rights reserved.'}
     </footer>
   );
-};
-
-export default Footer;
+}
