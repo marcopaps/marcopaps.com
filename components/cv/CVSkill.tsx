88e042c913ee85406421e9a0f617e477bbb85874
Marco Budiongan <marcobdngn@gmail.com>
2022-03-24 11:47:13 +0800

    Refactor cv skill

--- components/cv/CVSkill.tsx
+++ components/cv/CVSkill.tsx
@@ -1,9 +1,8 @@
 import { useMemo } from 'react';
 
 export enum RatingEnum {
-  'No experience at all',
+  '',
   'Beginner',
-  'Junior',
   'Average',
   'Experienced',
   'Expert',
@@ -16,25 +15,22 @@ interface IProps {
 }
 
 const Skill = (props: IProps) => {
-  /* 'w-1/5', 'w-2/5', 'w-3/5', 'w-4/5' */
+  /* 'w-1/4', 'w-2/4', 'w-3/4', 'w-4/4' */
   const width = useMemo(() => {
-    return props.rating != RatingEnum.Expert ? `w-${props.rating}/5` : 'w-full';
+    return props.rating != RatingEnum.Expert ? `w-${props.rating}/4` : 'w-full';
   }, [props.rating]);
 
-  const ratingTextColor = useMemo(() => {
-    return props.rating < 3 ? 'text-purple-900' : 'text-gray-100';
-  }, [props.rating]);
   return (
     <div className={props.className}>
       <div className="pt-2 font-bold">{props.label}</div>
       <div className="absolute h-5 w-full rounded-lg bg-gray-100">
-        <div
-          className={`absolute z-10 flex h-5 w-full items-center justify-center rounded-lg text-center text-xs ${ratingTextColor}`}
-        >
-          {RatingEnum[props.rating]}
-        </div>
+        <div className={`absolute z-10 h-5 w-full rounded-lg`}></div>
       </div>
-      <div className={`absolute h-5 rounded-lg bg-purple-900 ${width}`} />
+      <div
+        className={`absolute flex h-5 items-center justify-center rounded-lg bg-purple-900 text-center text-xs text-gray-100 ${width}`}
+      >
+        {RatingEnum[props.rating]}
+      </div>
     </div>
   );
 };
