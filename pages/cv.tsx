7793228fc004d8946ed64ddee31fb9c2889b6d85
Marco Budiongan <marcobdngn@gmail.com>
2022-03-24 14:13:26 +0800

    Refactor cv page

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, memo } from 'react';
 import { client, entries } from '@/utils/contentful';
 import { addSiteVisitCount } from '@/utils/common';
 import { Footer } from '@/components/common';
@@ -48,81 +47,57 @@ export async function getStaticProps() {
 }
 
 const Resume: NextPage<IProps> = (props) => {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-
   useEffect(() => {
-    const handleStart = (url: string) => {
-      url !== router.pathname ? setLoading(true) : setLoading(false);
-    };
-    const handleComplete = (url: string) => {
-      if (process.env.NODE_ENV === 'production') {
-        addSiteVisitCount();
-      }
-      setLoading(false);
-    };
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-  }, [router]);
+    if (process.env.NODE_ENV === 'production') {
+      addSiteVisitCount();
+    }
+  }, []);
 
   return (
-    <>
-      {loading || !props ? (
-        <div className="w-full py-48 text-center text-gray-800">
-          {'Loading...'}
-        </div>
-      ) : (
-        <div>
-          <Head>
-            <title>Marco Budiongan</title>
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width"
-            />
-            <meta name="description" content="Marco Budiongan"></meta>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-          </Head>
+    <div>
+      <Head>
+        <title>Marco Budiongan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Marco Budiongan"></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+      </Head>
 
-          {/* Header */}
+      {/* Header */}
 
-          <CVHeader className="sticky inset-0 z-20" />
+      <CVHeader className="sticky inset-0 z-20" />
 
-          <div className="grid grid-cols-1 px-6 md:my-8 md:grid-cols-3 lg:px-24 xl:px-48">
-            {/* Job experiences and profile intro section */}
+      <div className="grid grid-cols-1 px-6 md:my-8 md:grid-cols-3 lg:px-24 xl:px-48">
+        {/* Job experiences and profile intro section */}
 
-            <div className="mt-8 md:col-span-2 md:m-4">
-              <CVSection title="Profile">
-                <div className="font-lg text-md block items-center text-gray-800 md:text-lg">
-                  {
-                    'I am a full stack JS developer experienced in leveraging agile frameworks. Has experience working  in various projects for both corporate and start-up companies.'
-                  }
-                </div>
-              </CVSection>
-              <CVSection title="Employment" hr>
-                <CVJobExperiences jobExperiences={props.jobExperiences} />
-              </CVSection>
-            </div>
-
-            {/* Personal details */}
-
-            <div className="md:border-l-2 md:p-4">
+        <div className="mt-8 md:col-span-2 md:m-4">
+          <CVSection title="Profile">
+            <div className="font-lg text-md block items-center text-gray-800 md:text-lg">
               {
-                <CVPersonalDetails
-                  skills={props.skills}
-                  hobbies={props.hobbies}
-                  socialLinks={props.socialLinks}
-                />
+                'I am a full stack JS developer experienced in leveraging agile frameworks. Has experience working  in various projects for both corporate and start-up companies.'
               }
             </div>
-          </div>
-          <Footer />
+          </CVSection>
+          <CVSection title="Employment" hr>
+            <CVJobExperiences jobExperiences={props.jobExperiences} />
+          </CVSection>
         </div>
-      )}
-    </>
+
+        {/* Personal details */}
+
+        <div className="md:border-l-2 md:p-4">
+          {
+            <CVPersonalDetails
+              skills={props.skills}
+              hobbies={props.hobbies}
+              socialLinks={props.socialLinks}
+            />
+          }
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
-export default Resume;
+export default memo(Resume);
--- utils/common/addSiteVisitCount.ts
+++ utils/common/addSiteVisitCount.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default async function useAddSiteVisitCount() {
+export default async function addSiteVisitCount() {
   const endpoint = process.env.SERVERLESS_ENDPOINT;
   const serverlessFunction = process.env.SERVERLESS_SITE_COUNTER_FUNCTION;
 
